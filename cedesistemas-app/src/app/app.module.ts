434e523830b269886ae230489d7bfae8dcf9f94d
Andrés Londoño <andres.londono@teaminternational.com>
2019-06-22 10:56:11 -0500

    Implementacion de angular material y componente de header

--- cedesistemas-app/src/app/app.module.ts
+++ cedesistemas-app/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-component/app.component';
+import { MaterialModule } from './material.module';
+
+import { HeaderComponent } from './header-component/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cedesistemas-app/src/app/header-component/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
+
+@Component({
+    selector: 'header-app',
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
+
+
+ 
